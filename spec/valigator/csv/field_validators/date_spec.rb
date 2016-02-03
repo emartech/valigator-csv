4b324f99181c3ddc03b666b21718db13606aa25e
thilonel <nagy.daniel@emarsys.com>
2016-02-03 11:10:03 +0100

    test: field validator date #valid

--- spec/valigator/csv/field_validators/date_spec.rb
+++ spec/valigator/csv/field_validators/date_spec.rb
@@ -8,6 +8,22 @@ module Valigator
         describe "#valid?" do
           {
             "2014-04-11" => true,
+            "April 11, 2014" => true,
+            "01/02/03" => true,
+            "1999-Jan-08" => true,
+            "Jan-08-1999" => true,
+            "08-Jan-1999" => true,
+            "08-Jan-99" => true,
+            "Jan-08-99" => true,
+            "19990108" => true,
+            "990108" => true,
+            "J2451187" => true,
+            "January 8, 99 BC" => true,
+
+            "1999.008" => false,
+            "99-Jan-08" => false,
+            "4/18/2014" => false,
+            "1/18/1999" => false,
             "2014-04-31" => false,
             "not-a-date" => false,
             nil => false,
@@ -38,11 +54,11 @@ module Valigator
             subject { described_class.new allow_blank: true }
 
             {
+              nil => true,
+              "" => true,
               "2014-04-11" => true,
               "2014-04-31" => false,
-              "not-a-date" => false,
-              nil => true,
-              "" => true
+              "not-a-date" => false
             }.each do |input, output|
               it "returns #{output} for value: #{input.inspect}" do
                 expect(subject.valid?(input)).to eq(output)
